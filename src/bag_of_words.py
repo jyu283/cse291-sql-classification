209fc8c08fc0218d41412c10fe712686fca1c71b
Jerry Yu <jerryyu01@outlook.com>
2024-02-13 21:42:37 -0800

    pandas.DataFrame

--- src/bag_of_words.py
+++ src/bag_of_words.py
@@ -4,15 +4,19 @@ Produce bag-of-words from query CSV files (see data/*.csv).
 
 import csv
 import sys
-
+import pandas as pd
 from collections import namedtuple, OrderedDict
-
 from typing import TextIO, List, Set, Tuple
 
 QueryRecord = namedtuple("QueryRecord", ["id", "text", "time"])
 
 
 class BagOfWords:
+    """
+    Process a query result CSV file (currently only in Snowflake export format)
+    into a bag-of-words.
+    """
+
     def __init__(self, csv_file: TextIO):
         self.csv_file = csv_file
 
@@ -58,8 +62,6 @@ class BagOfWords:
             total_tokens += len(tokens)
             bag.update(self.tokenize_query(text))
 
-        print("Bag of words: ")
-        print(bag)
         print(f"{len(bag)} unique tokens from {total_tokens} total.")
 
         return bag
@@ -73,12 +75,36 @@ class BagOfWords:
             vector[token] += 1
         return vector
 
-    def process(self):
+    def process(self) -> List[pd.DataFrame]:
+        """
+        Process CSV file into a list of pandas DataFrame objects.
+        Each DataFrame contains:
+        {
+            "query_id": [query id],
+            "query_text": [original SQL query statement],
+            "elapsed_time": [measured query execution time],
+            "bow_vector": {
+                "word1": [occurrence],
+                "word2": [occurrence],
+                "word3": [occurrence],
+                ...
+            }
+        }
+        """
         self.bag = self.compute_bag()
+        data: List[pd.DataFrame] = []
         for query_id, query_text, elapsed_time in self.rows:
             vector = self.vectorize_query(query_text)
-            vectorstr = str(list(vector.values()))
-            print(query_id, vectorstr, elapsed_time)
+            df = pd.DataFrame(
+                {
+                    "query_id": query_id,
+                    "query_text": query_text,
+                    "elapsed_time": elapsed_time,
+                    "bow_vector": vector,
+                }
+            )
+            data.append(df)
+        return data
 
 
 if __name__ == "__main__":
@@ -87,4 +113,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     bow = BagOfWords(sys.argv[1])
-    bow.process()
+    data = bow.process()
