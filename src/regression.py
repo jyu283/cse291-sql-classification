478cab79f28b687b2f62ff998077d75d2da0f3ec
Jiaying6429 <77264976+Jiaying6429@users.noreply.github.com>
2024-02-16 07:05:03 +0800

    Update regression.py

--- src/regression.py
+++ src/regression.py
@@ -8,7 +8,7 @@ from collections import Counter
 import sys
 
 class SQLQueryPerformanceClassifier:
-    def __init__(self, csv_path, threshold=60000):
+    def __init__(self, csv_path, threshold=14000):
         self.csv_path = csv_path
         self.threshold = threshold
         self.vectorizer = CountVectorizer()
