f410a7f884e25199adf08068e19639b676e3ebf9
Jerry Yu <jiy066@ucsd.edu>
2024-03-11 19:52:18 -0700

    new csv format

--- src/query_plan_parser.py
+++ src/query_plan_parser.py
@@ -2,36 +2,39 @@ import sys
 import csv
 import json
 import pprint
+from collections import namedtuple
+
+QueryRecord = namedtuple("QueryRecord", ["id", "text", "time", "plan"])
 
 
 class QueryPlanParser:
-    def __init__(self, csv_file: str, query_ids_file: str):
-        self.query_plans = dict()
-
-        query_ids = self.get_query_ids_from_file(query_ids_file)
+    def __init__(self, csv_file: str):
+        self.queries = []
 
         with open(csv_file, "r") as csvfile:
             reader = csv.DictReader(csvfile)
-            i = 0
             for row in reader:
-                query_id = query_ids[i]
-                raw_json = row.get("content")
-                query_plan = json.loads(raw_json)
-                self.query_plans[query_id] = query_plan
-                i += 1
-            pprint.pprint(self.query_plans)
+                query_id = row.get("QUERY_ID")
+                query_plan = json.loads(row.get("content"))
+                query_text = row.get("QUERY_TEXT").replace("explain using json ", "")
+                query_time = row.get("TOTAL_ELAPSED_TIME")
+                self.queries.append(
+                    QueryRecord(
+                        id=query_id,
+                        text=query_text,
+                        time=query_time,
+                        plan=query_plan,
+                    )
+                )
+            pprint.pprint(self.queries)
 
-    def get_query_ids_from_file(self, query_ids_file: str):
-        with open(query_ids_file, "r") as fp:
-            lines = fp.readlines()
-            for i in range(len(lines)):
-                lines[i] = lines[i].strip()
-            return lines
+    def vectorize_query(self, query: str, query_plan: dict):
+        pass
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python query_plan_parser.py queryplan.json query_ids_file")
+    if len(sys.argv) != 2:
+        print("Usage: python query_plan_parser.py queryplan.json")
         exit(1)
 
-    parser = QueryPlanParser(sys.argv[1], sys.argv[2])
+    parser = QueryPlanParser(sys.argv[1])
